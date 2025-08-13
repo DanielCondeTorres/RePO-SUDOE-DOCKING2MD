0a6724403bc1151a0d8581b0a2396b04ebed0178
dani <danielconde.torres@usc.es>
2025-08-13 18:15:07 +0200

    actualizacion VR_recptor_proteina

--- Code/Working_Area/VR/bfactor.sh
+++ Code/Working_Area/VR/bfactor.sh
@@ -31,10 +31,21 @@ mkdir -p "$vr_folder"
 gmx convert-trj -f "$path/min_fep1.trr" -o "$path/min_fep1_c.xtc"
 
 gmx trjcat -f "$path/min_fep1_c.xtc" "$path/eq_nvt_fep.xtc" "$path/eq_npt_fep_1.xtc" "$path/prod.xtc" -o "$path/traj.xtc"
-echo -e "q" | gmx make_ndx -f "$path/prod.tpr" -o "$path/index.ndx"
-echo -e "Protein\nOther\nq" | gmx trjconv -s "$path/prod.tpr" -f "$path/traj.xtc" -pbc mol -center -n "$path/index.ndx" -o "$path/center_complete.pdb" -e 0
+gmx make_ndx -f "$path/prod.tpr" -o "$path/index.ndx" << EOF
+ "Protein" | "Other"
+q
+EOF
+gmx trjconv -s "$path/prod.tpr" -f "$path/traj.xtc" -pbc mol -center -n "$path/index.ndx" -o "$path/center_complete.pdb" -e 0 << EOF
+Protein 
+Protein_Other
+q
+EOF
 
-echo -e "Protein\nOther\nq" | gmx trjconv -s "$path/prod.tpr" -f "$path/traj.xtc" -pbc mol -center -n "$path/index.ndx" -o "$path/center_complete.xtc" -b 0
+gmx trjconv -s "$path/prod.tpr" -f "$path/traj.xtc" -pbc mol -center -n "$path/index.ndx" -o "$path/center_complete.xtc" -b 0 << EOF
+Protein 
+Protein_Other
+q
+EOF
 
 # Execute the Python script
 python VR/ligand_energy_attribution_bfactor.py \
